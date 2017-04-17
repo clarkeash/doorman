73ccf07e3ed735e875b44be3a1cd6ca98f6395af
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-04-17 21:01:00 +0200

    Attemp to change table name from config

--- src/Models/Invite.php
+++ src/Models/Invite.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invite extends Model
 {
-    protected $table = config('doorman.invite_table_name');
     protected $dates = [ 'valid_until' ];
+    $this->setTable(config('doorman.invite_table_name'));
 }
