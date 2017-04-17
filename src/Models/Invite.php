6c3901e1a410ddb6294f69df973e0c79e7592d81
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-04-17 21:17:57 +0200

    Use the given table name on migrations (#6)
    use config value for table name - in migrations and on the model.

--- src/Models/Invite.php
+++ src/Models/Invite.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Invite extends Model
 {
     protected $dates = [ 'valid_until' ];
+    
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('doorman.invite_table_name');
+        parent::__construct($attributes);
+    }
 }
