264d454f1f369ba06ced00e0562d33722079598e
Erik Campobadal <soc@erik.cat>
2017-05-01 14:31:16 +0200

    Added catalan translations

---
+++ resources/translations/ca/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Doorman Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for error messages.
+    |
+    */
+
+    'expired' => 'La invitació :CODE ha caducat.',
+    'invalid' => 'La invitació :CODE és invàlida.',
+    'maxed' => 'La invitació :CODE ja ha sigut usada totes les vegades permeses.',
+    'restricted' => 'La invitació :CODE pertany a un altre usuari.',
+];
