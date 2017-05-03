24f03517edd0e47e7d513aa4abdfe2232949a9fd
Ashley Clarke <me@ashleyclarke.me>
2017-05-03 20:24:08 +0200

    remove useless constructor

--- src/Commands/CleanupCommand.php
+++ src/Commands/CleanupCommand.php
@@ -22,16 +22,6 @@ class CleanupCommand extends Command
     protected $description = 'Remove expired invites from the database.';
     
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-    
-    /**
      * Execute the console command.
      *
      * @return mixed
